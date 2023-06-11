aa507fad8775b243c7ea6b4b5b657bf1f47fa9d1
Yulia <yuliagub07@gmail.com>
2023-06-11 13:55:15 +0300

    binding data

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { DatePicker } from "tns-core-modules/ui/date-picker";
+//import { DatePicker } from "tns-core-modules/ui/date-picker";
 
-import { Item } from './item'
+//import { Item } from './item'
 import { ItemService } from './item.service'
 
 @Component({
@@ -10,37 +10,16 @@ import { ItemService } from './item.service'
 })
 
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
-  minDate: Date = new Date(1975, 0, 29);
-  maxDate: Date = new Date(2045, 4, 12);
+  myName : string;
 
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
+    this.myName = "Yulia";
   }
-  onDatePickerLoaded(args) {
-    // const datePicker = args.object as DatePicker;
-}
-
-onDateChanged(args) {
-    console.log("Date New value: " + args.value);
-    console.log("Date value: " + args.oldValue);
-}
-
-onDayChanged(args) {
-    console.log("Day New value: " + args.value);
-    console.log("Day Old value: " + args.oldValue);
-}
-
-onMonthChanged(args) {
-    console.log("Month New value: " + args.value);
-    console.log("Month Old value: " + args.oldValue);
-}
-
-onYearChanged(args) {
-    console.log("Year New value: " + args.value);
-    console.log("Year Old value: " + args.oldValue);
-}
+  btnClicked(evt){
+    console.log(evt)
+    console.log("Button Clicked")
+  }
 }
 
